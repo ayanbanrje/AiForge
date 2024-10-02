ecb414550485ce4d5d441309532a5517a6237b67
Rauniyar Badri Prasad (SDS/EDS) <bqu1kor@bosch.com>
2024-10-02 19:50:14 +0530

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,48 +1,42 @@
-// src/components/Header/Header.js
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 import Colors from '../../../assets/constant/colors';
+
+const { width } = Dimensions.get('window'); // Get the device's width
 
 const Header = ({ title }) => {
     return (
         <View style={styles.header}>
-            {<Image
-                source={require("../../assets/images/supergraphic.png")}
-            />}
+            <Image source={require("../../assets/images/supergraphic.png")} style={styles.supergraphic} />
             <View style={styles.content}>
                 <View style={styles.leftSideContent}>
-                    {<Image
+                    <Image
                         source={require("../../assets/images/Bosch_COLOR_RGB.png")}
                         style={styles.logo}
-                    />}
+                    />
                     <View style={styles.separator} />
-
                     <Text style={styles.textBlueBold}>Genasis</Text>
                 </View>
                 <View style={styles.leftSideContent}>
-                    {<Image
+                    <Image
                         source={require("../../../assets/images/question-frame.png")}
-                    />}
+                        style={styles.icon} // Ensure images are responsive
+                    />
                     <Text style={styles.textBlackNormal}>Help</Text>
-                    {<Image
+                    <Image
                         source={require("../../../assets/images/my-brand-frame.png")}
-                    />}
+                        style={styles.icon} // Ensure images are responsive
+                    />
                     <Text style={styles.textBlackNormal}>Sanjeev Sayani</Text>
-
                 </View>
             </View>
-
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     header: {
-        display: "flex",
         flexDirection: "column",
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        // backgroundColor: '#f8f8f8',
         borderBottomWidth: 1,
         borderBottomColor: '#e1e1e1',
     },
@@ -50,18 +44,16 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
-        paddingHorizontal: 32,
-        paddingVertical: 8
+        paddingHorizontal: width * 0.01, // 8% of screen width
+        paddingVertical: width * 0.005, // 2% of screen width
     },
     separator: {
-        marginHorizontal: 16,
+        marginHorizontal: width * 0.01,
         width: 2, // Height of the horizontal bar
-        height: 20, // Full width of the container
-        backgroundColor: 'gray', // Color of the separator
-        // marginVertical: 10, /
+        height: 20,
+        backgroundColor: 'gray',
     },
     leftSideContent: {
-        display: "flex",
         flexDirection: "row",
         alignItems: "center",
     },
@@ -75,10 +67,19 @@ const styles = StyleSheet.create({
         fontFamily:"BoschSans",
         // fontWeight: 'bold',
         color: 'black',
-        marginHorizontal:16
+        marginHorizontal: width * 0.01,
     },
     logo: {
         alignItems: 'left',
+        
+    },
+    icon: {
+        width: 30, // Adjust size as necessary
+        height: 30, // Adjust size as necessary
+    },
+    supergraphic: {
+        width: '100%', // Ensure full width for the supergraphic
+        
     }
 });
 
